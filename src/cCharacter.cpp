3ddd6f4b06e72af336f3d1938125d0055ab627e3
ebroto <ebroto@gmail.com>
2013-12-13 23:22:12 +0100

    add character and enemy

---
+++ src/cCharacter.cpp
@@ -0,0 +1,95 @@
+#include "cCharacter.h"
+
+cCharacter::cCharacter() :
+	cBaseEntity()
+{
+
+}
+
+cCharacter::cCharacter(const std::string& textureId, const cRectangle& textureRect, s32 x, s32 y, s32 z, s32 vStraight, s32 vDiagonal, f32 textureScale)
+{
+	Init(textureId, textureRect, x, y, z, vStraight, vDiagonal, textureScale);
+}
+
+cCharacter::~cCharacter()
+{
+
+}
+
+void cCharacter::Init(const std::string& textureId, const cRectangle& textureRect, s32 x, s32 y, s32 z, s32 vStraight, s32 vDiagonal, f32 textureScale)
+{
+	SetPosition(x, y);
+	SetZIndex(z);
+	SetTextureID(textureId);
+	SetTextureRect(textureRect);
+	SetZIndex(z);
+	EnableCollision();
+	SetCollisionRectRelative(textureRect);
+	SetTextureScale(textureScale);
+
+	m_vStraight = vStraight;
+	m_vDiagonal = vDiagonal;
+}
+
+void cCharacter::Move(s32 xAmount, s32 yAmount)
+{
+	if (xAmount || yAmount)
+	{
+		// Busco l'orientacio del moviment
+		if (yAmount == 0)
+		{
+			if (xAmount > 0)		m_orientation = ORIENTATION_E;
+			else if (xAmount < 0)	m_orientation = ORIENTATION_O;
+		}
+		else if (yAmount > 0)
+		{
+			if (xAmount > 0)		m_orientation = ORIENTATION_SE;
+			else if (xAmount < 0)	m_orientation = ORIENTATION_SO;
+			else				m_orientation = ORIENTATION_S;
+		}
+		else
+		{
+			if (xAmount > 0)		m_orientation = ORIENTATION_NE;
+			else if (xAmount < 0)	m_orientation = ORIENTATION_NO;
+			else				m_orientation = ORIENTATION_N;
+		}
+
+		int x, y;
+		GetPosition(x, y);
+
+		//Actualitzo el moviment segons orientacio
+		switch (m_orientation)
+		{
+		case ORIENTATION_N:
+			y -= m_vStraight;
+			break;
+		case ORIENTATION_NE:
+			y -= m_vDiagonal;
+			x += m_vDiagonal;
+			break;
+		case ORIENTATION_NO:
+			y -= m_vDiagonal;
+			x -= m_vDiagonal;
+			break;
+		case ORIENTATION_S:
+			y += m_vStraight;
+			break;
+		case ORIENTATION_SE:
+			y += m_vDiagonal;
+			x += m_vDiagonal;
+			break;
+		case ORIENTATION_SO:
+			y += m_vDiagonal;
+			x -= m_vDiagonal;
+			break;
+		case ORIENTATION_E:
+			x += m_vStraight;
+			break;
+		case ORIENTATION_O:
+			x -= m_vStraight;
+			break;
+		}
+
+		SetPosition(x, y);
+	}
+}
